cdd9e6109278f6e811e5a29be36a5ee0a367b57a
Trison Nguyen <trison.ng@gmail.com>
2018-03-30 21:07:10 -0700

    GET single user

--- server.js
+++ server.js
@@ -94,6 +94,14 @@ apiRouter.route('/users')
 		});
 	})
 	
+//user
+apiRouter.route('/users/:user_id')
+	.get(function(req, res){
+		User.findById(req.params.user_id, function(err, user){
+			if (err) res.send(err);
+			res.json(user);
+		});
+	})
 
 //register routes
 app.use('/api', apiRouter);
